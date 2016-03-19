954398c3c073e83179f49323c20e23aec173c521
njlordi <njlordi@gmail.com>
2016-03-18 23:28:20 -0400

    Added source code and txt file

---
+++ ReadFromFile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadFromFile : MonoBehaviour {
+	public string allText;
+	
+	void Start () {
+		System.IO.StreamReader MyReader = new System.IO.StreamReader("C:\\helloworld.txt");
+		while((allText = MyReader.ReadLine()) != null)
+		{
+			Debug.Log (allText);
+		}
+		MyReader.Close();
+	}
+}
